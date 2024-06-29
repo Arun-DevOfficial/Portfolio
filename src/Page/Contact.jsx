0c561ec3863aa2fa5e0feadf6961bc56b0579caf
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-06-29 20:16:48 +0530

    Update map feature

--- src/Page/Contact.jsx
+++ src/Page/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+// import React from 'react';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <iframe
+        title="Google Maps"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d66208.13871516123!2d79.53165441170692!3d11.781937353727047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a54af5a3b3e8267%3A0xb41239c6f2ec332f!2sPanruti%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1719671925873!5m2!1sen!2sin"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        className="rounded-xl"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
 }
