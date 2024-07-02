b416fb23d32c69ec60835dad3234d25fdd2483ee
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-07-02 12:59:50 +0530

    Add Resume Page

--- src/Page/Resume.jsx
+++ src/Page/Resume.jsx
@@ -1,7 +1,182 @@
-import React from 'react'
+// import React from 'react'
 
 export default function Resume() {
   return (
-    <div>Resume</div>
-  )
+    <>
+      <section>
+        <div>
+          <h1 className="font-bold text-4xl">Portfolio</h1>
+          <div className="bg-[#FEC562] h-1 rounded-full w-20 my-3"></div>
+        </div>
+
+        {/* Experiences Section */}
+        <div className="min-h-fit p-5">
+          <div className="mx-auto flex max-w-4xl flex-col gap-8 text-white">
+            <h2 className="text-3xl font-semibold">Experiences</h2>
+            <ul className="flex max-w-lg flex-col">
+              <li className="spy relative flex items-start gap-6 py-3">
+                <div className="w-12 rounded-xl bg-[#1f1f21] shadow p-3 border-l border-jet before:absolute before:left-[24.2px] before:top-[62px] before:h-full before:w-[2px] before:bg-[#363636]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6 text-[#e3c466]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h3 className="text-xl font-medium text-white">
+                    Assistant Technical Trainer
+                  </h3>
+                  <p className="py-3 text-sm capitalize text-light-gray-70">
+                    Embvs Enterprises - <span>11 Apr 2023</span>
+                  </p>
+                  <p className="text-light-gray-70">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Expedita a porro praesentium rem architecto.
+                  </p>
+                </div>
+              </li>
+              <li className="spy relative flex items-start gap-6 py-3">
+                <div className="w-12 rounded-xl bg-[#1f1f21] shadow p-3 border-l border-jet before:absolute before:left-[24.2px] before:top-[62px] before:h-full before:w-[2px] before:bg-[#363636]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6 text-[#e3c466]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h3 className="text-xl font-medium text-white">
+                    Assistant Technical Trainer
+                  </h3>
+                  <p className="py-3 text-sm capitalize text-light-gray-70">
+                    Embvs Enterprises - <span>11 Apr 2023</span>
+                  </p>
+                  <p className="text-light-gray-70">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Expedita a porro praesentium rem architecto.
+                  </p>
+                </div>
+              </li>
+              <li className="spy relative flex items-start gap-6 py-3">
+                <div className="w-12 rounded-xl bg-[#1f1f21] shadow p-3 border-l border-jet">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6 text-[#e3c466]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h3 className="text-xl font-medium text-white">
+                    Assistant Technical Trainer
+                  </h3>
+                  <p className="py-3 text-sm capitalize text-light-gray-70">
+                    Embvs Enterprises - <span>11 Apr 2023</span>
+                  </p>
+                  <p className="text-light-gray-70">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Expedita a porro praesentium rem architecto.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Education Section */}
+        <div className="min-h-fit p-5">
+          <div className="mx-auto flex max-w-4xl flex-col gap-8 text-white">
+            <h2 className="text-3xl font-semibold">Education</h2>
+            <ul className="flex max-w-lg flex-col">
+              <li className="spy relative flex items-start gap-6 py-3">
+                <div className="w-12 rounded-xl bg-[#1f1f21] shadow p-3 border-l border-jet before:absolute before:left-[24.2px] before:top-[62px] before:h-full before:w-[2px] before:bg-[#363636]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6 text-[#e3c466]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h3 className="text-xl font-medium text-white">
+                    Assistant Technical Trainer
+                  </h3>
+                  <p className="py-3 text-sm capitalize text-light-gray-70">
+                    Embvs Enterprises - <span>11 Apr 2023</span>
+                  </p>
+                  <p className="text-light-gray-70">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Expedita a porro praesentium rem architecto.
+                  </p>
+                </div>
+              </li>
+              <li className="spy relative flex items-start gap-6 py-3">
+                <div className="w-12 rounded-xl bg-[#1f1f21] shadow p-3 border-l border-jet">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6 text-[#e3c466]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h3 className="text-xl font-medium text-white">
+                    Assistant Technical Trainer
+                  </h3>
+                  <p className="py-3 text-sm capitalize text-light-gray-70">
+                    Embvs Enterprises - <span>11 Apr 2023</span>
+                  </p>
+                  <p className="text-light-gray-70">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Expedita a porro praesentium rem architecto.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
